18e0422957672d03f7954d9c16afd9c7d3825e0a
Eisenbahnwagen451 <vitlog@gmail.com>
2016-09-16 21:36:08 +0200

    first commit main.cpp added!

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16.09.2016
+ *      Author: 451
+ *      git remote repository:
+ *     		https://github.com/Eisenbahnwagen451/hello-world.git
+ */
+#include <iostream>
+int main() {
+	std::cout << "Hope is the first step on the road to disappointment" << std::endl;
+
+	return 0;
+}
+
+
